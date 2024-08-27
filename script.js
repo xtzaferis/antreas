b190d5397abf7acf68d1c901f0b5e34a59aabd84
xtzaferis <xtzaferis@gmail.com>
2024-08-27 23:54:51 +0300

    footer and styling

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ burger.addEventListener('click', () => {
 window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 70);
 });
+
+function goToTheTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
